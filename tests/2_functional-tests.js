343e1cfc4b9068b290eb4ac9237598d71a0f9537
Valloice <vinhs1605@gmail.com>
2022-01-25 12:26:51 -1200

    Attempt#1

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -15,8 +15,8 @@ suite('Functional Tests', function () {
         .request(server)
         .get('/hello')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello Guest');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Guest');
           done();
         });
     });
@@ -24,10 +24,10 @@ suite('Functional Tests', function () {
     test('Test GET /hello with your name', function (done) {
       chai
         .request(server)
-        .get('/hello?name=xy_z')
+        .get('/hello?name=Vince')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello xy_z');
+          assert.equal(res.status, 200);
+          assert.equal(res.text, 'hello Vince');
           done();
         });
     });
@@ -36,29 +36,51 @@ suite('Functional Tests', function () {
       chai
         .request(server)
         .put('/travellers')
-
+        .send({
+          "surname": "Colombo"
+        })
         .end(function (err, res) {
-          assert.fail();
-
+          assert.equal(res.status, 200)
+          assert.equal(res.type, "application/json")
+          assert.equal(res.body.name, "Cristoforo")
+          assert.equal(res.body.surname,"Colombo")
           done();
         });
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
-
+      chai
+        .request(server)
+        .put('/travellers')
+        .send({
+          surname: "da Verrazzano"
+        })
+        .end((err, res) => {
+          assert.equal(res.status, 200)
+          assert.equal(res.type, "application/json")
+          assert.equal(res.body.name, "Giovanni")
+          assert.equal(res.body.surname,"da Verrazzano")
+        })
       done();
     });
   });
 });
 
+//Why zombie wtf, nvm
 const Browser = require('zombie');
+Browser.site = "http://localhost:3000/"
+
+let browser = new Browser()
+
+suiteSetup((done) => {
+  return browser.visit("/", done)
+})
 
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
 
 
-
+//I see, headless, is that the reason why zombie?
   suite('Headless browser', function () {
     test('should have a working "site" property', function() {
       assert.isNotNull(browser.site);
@@ -68,15 +90,27 @@ suite('Functional Tests with Zombie.js', function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill("surname", "Colombo").then(() => {
+        browser.pressButton("submit", () => {
+          browser.assert.success()
+          browser.assert.text("span#name", "Cristoforo")
+          browser.assert.text("span#surname", "Colombo")
+          browser.assert.elements("span#dates", 1);
+          done();
+        })
+      })
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill('surname', 'Vespucci').then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success()
+          browser.assert.text('span#name', "Amerigo")
+          browser.assert.text("span#surname", "Vespucci")
+          browser.assert.elements("span#dates", 1)
+          done()
+        })
+      })
     });
   });
 });
